2f8fd45d562298e1c8cd66f93dceef28810e5672
M Fajar Ali <fajaralim678@gmail.com>
2022-06-07 11:28:50 +0700

    UI: product card (#8)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,13 +4,22 @@ import Nav from "components/nav";
 import UnggahResep from "components/unggahResep";
 import Footer from "components/Footer";
 import MetodePembayaran from "components/MetodePembayaran";
+import ProductCard from "components/ProductCard";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
     <Box>
-      <Nav />
-      <UnggahResep />
-      <MetodePembayaran />
+    <Nav />
+      <Container>
+        <Button variant="contain">hello world</Button>
+        <Typography variant="h2">hello world</Typography>
+        <ProductCard />
+        <UnggahResep/>
+        <MetodePembayaran />
+      </Container>
       <Footer />
     </Box>
   );
