cca172679cbf0b017f4a792bed0ff06a664b05d4
M Fajar Ali <fajaralim678@gmail.com>
2022-07-25 14:23:24 +0700

    fix: fix the initial GA error (#88)
    * Feat: added google analytics
    
    * added page title
    
    * UI: show products images
    
    * added GA for add to cart button
    
    * fix: fix delete cart modal
    
    * delete console log
    
    * added protection to GA

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,15 +1,19 @@
 import { gaMeasurementId } from "../config/ga";
 
 export const pageView = (url) => {
-  window.gtag("config", gaMeasurementId, {
-    page_path: url,
-  });
+  if (typeof window !== "undefined") {
+    window.gtag("config", gaMeasurementId, {
+      page_path: url,
+    });
+  }
 };
 
 export const gaEvent = ({ action, label, value, category }) => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value,
-  });
+  if (typeof window !== "undefined") {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value,
+    });
+  }
 };
